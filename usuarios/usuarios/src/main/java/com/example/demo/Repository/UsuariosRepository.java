6adafb8e0edc2d667b8163db0fb6023904e70605
KikiRodriguez <sankikiller@hotmail.com>
2024-07-07 15:03:30 -0300

    Delete usuarios/usuarios directory

--- usuarios/usuarios/src/main/java/com/example/demo/Repository/UsuariosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Model.Usuarios;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuariosRepository extends JpaRepository <Usuarios, Long> {
-
-}
